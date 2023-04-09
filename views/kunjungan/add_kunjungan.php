03eb82fee05270523151545837606b927f9191ce
uba.pbt <uba.pbt@gmail.com>
2023-04-09 22:30:15 +0800

    kunjungan 2 20230409

---
+++ views/kunjungan/add_kunjungan.php
@@ -0,0 +1,68 @@
+<?php
+include("../../config/connection.php");
+
+if (isset($_POST["act_kunjungan"])) {
+
+    $id_tamu = $_POST['nik'];
+    $keperluan = $_POST['keperluan'];
+    $pej_temu = '';
+    $tgl_kunjung = $_POST['tgl_kunjung'];
+    $jam_kunjung = $_POST['jam_kunjung'];
+    $jml_rombongan = $_POST['jml_rombongan'];
+
+    // var_dump($id_permintaan);
+
+
+    $query = "INSERT INTO kunjungan(id_tamu,keperluan,pej_temu,tgl_kunjung,jam_kunjung,jml_rombongan)
+    values('$id_tamu','$keperluan','$pej_temu','$tgl_kunjung','$jam_kunjung','$jml_rombongan')";
+    echo "$query";
+    // die();
+    $res = mysqli_query($connect, $query);
+    var_dump($connect);
+
+    if ($res) {
+        echo "<script>console.log('data sukses disimpan')</script>";
+        // header("location:index.php?permintaan");
+    } else {
+        echo "<script>console.log('data gagal disimpan')</script>";
+    }
+} 
+
+?>
+
+<form id="" action="#">
+                        <div class="field">
+              <label class="label">NIK</label>
+              <div class="control">
+                <input class="input" type="text" name="nik" placeholder="Nik" id="nik">
+              </div>
+            <div class="field">
+              <label class="label">tgl_kunjung</label>
+              <div class="control">
+                <input class="input" type="date" name="tgl_kunjung" placeholder="tgl_kunjung" id="tgl_kunjung">
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">jam_kunjung</label>
+              <div class="control">
+                <input class="input" type="time" name="jam_kunjung" placeholder="Jam Kunjung" id="jam_kunjung">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">JML Rombongan</label>
+              <div class="control">
+                <input class="input" type="text" name="jml_rombongan" placeholder="jml_rombongan" id="jml_rombongan">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">keperluan</label>
+              <div class="control">
+                <input class="input" type="text" name="keperluan" placeholder="keperluan" id="keperluan">
+              </div>
+            </div>
+            <div class="control">
+              <input type="hidden" name="act_kunjungan" id="act_kunjungan" value="insert">
+              <a class="button is-primary" name="add_kunjungan" id="add_kunjungan">Simpan Kunjungan</a>
+            </div>
+          </form>
---
+++ views/tamu/add_tamu.php
@@ -0,0 +1,66 @@
+<?php
+include("../../config/connection.php");
+
+if (isset($_POST["action"])) {
+   
+$nik = $_POST['nik'];
+$nama = $_POST['nama'];
+$kabupaten = $_POST['kabupaten'];
+$satuan_kerja = $_POST['satuan_kerja'];
+$no_hp = $_POST['no_hp'];
+
+    $query = "INSERT INTO tamu(nik,nama,jk,alamat,provinsi,kabupaten,pekerjaan,satuan_kerja,no_hp)
+    values('$nik','$nama','','','','$kabupaten','','$satuan_kerja','$no_hp')";
+
+    // echo "<br>$query <br>";
+    // die();
+    $res = mysqli_query($connect, $query);
+    // var_dump($connect);
+    // echo "<br> $res <br>";
+
+
+    if ($res) {
+        echo "data berhasil disimpan";
+    } else {
+        echo "data gagal disimpan";
+    }
+}
+?>
+
+
+<form id="add_form" action="#">
+                        <div class="field">
+              <label class="label">NIK</label>
+              <div class="control">
+                <input class="input" type="text" name="nik" placeholder="Nik" id="nik">
+              </div>
+            <div class="field">
+              <label class="label">Nama</label>
+              <div class="control">
+                <input class="input" type="text" name="nama" placeholder="nama" id="nama">
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Satuan Kerja</label>
+              <div class="control">
+                <input class="input" type="text" name="satuan_kerja" placeholder="Satuan Kerja" id="satuan_kerja">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Kabupaten Asal</label>
+              <div class="control">
+                <input class="input" type="text" name="kabupaten" placeholder="Tempat Asal" id="kabupaten">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">No Hp</label>
+              <div class="control">
+                <input class="input" type="text" name="no_hp" placeholder="nohp" id="no_hp">
+              </div>
+            </div>
+            <div class="control">
+              <input type="hidden" name="action" id="action" value="insert">
+              <a class="button is-primary" name="add_tamu" id="add_tamu">Simpan Tamu</a>
+            </div>
+          </form>
