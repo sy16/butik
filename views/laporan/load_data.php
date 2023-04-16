91fdd291e18a1342b17bd8a4e04ef82de6e1eff9
uba.pbt <uba.pbt@gmail.com>
2023-04-16 10:11:33 +0800

    membuat form laporan

---
+++ views/laporan/load_data.php
@@ -0,0 +1,49 @@
+<?php
+
+include("../../config/connection.php");
+
+
+
+$sql = "SELECT tamu.nama, tamu.alamat, tamu.satuan_kerja, kunjungan.tgl_kunjung, kunjungan.jml_rombongan FROM tamu INNER JOIN kunjungan WHERE tamu.nik = kunjungan.id_tamu;";
+$res = mysqli_query($connect, $sql);
+?>
+
+<table class='table'>
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>Alamat</th>
+        <th>Satuan Kerja</th>
+        <th>Tgl Kunjungan</th>
+        <th>Jml Rombongan</th>
+      </tr>
+    </thead>
+
+<?php
+$no=1;
+while($row = mysqli_fetch_array($res)){
+
+?>
+
+    <tbody>
+    <tr>
+        <th><?= $no ?></th>
+        <td><?= $row['nama'] ?></td>
+        <td><?= $row['alamat'] ?></td>
+        <td><?= $row['satuan_kerja'] ?></td>
+        <td><?= date('d F Y', strtotime($row['tgl_kunjung'])) ?></td>
+        <td><?= $row['jml_rombongan'] ?></td>
+        
+    </tr>
+    </tbody>
+
+    <?php
+    $no++;
+}
+    ?>
+</table>
+
+<button type="button" class="btn btn-danger btn_cls_laporan">
+      Close Laporan
+    </button>
